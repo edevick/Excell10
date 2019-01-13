1e9d34443dbd0cc53e3492cc90c0a339882ac4b7
edevick <ekdevitskaya@gmail.com>
2019-01-14 00:13:53 +0200

    fixed methods for cell

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -12,23 +12,32 @@ public class Demo {
         schema.createSchema("Excell");
         Sheet sheet = new Sheet("Sheet");
         sheet.createSheet();
-        //sheet.addColumn(3);
-       // sheet.deleteColumn(1, 1);
-        sheet.addRow(5);
-      //  sheet.deleteRow(1, 2);
-     /*   List<Sheet> tables = new ArrayList();
+        sheet.addColumn(3);
+
+        // sheet.deleteColumn(1, 1);
+
+         sheet.addRow(5);
+
+        //  sheet.deleteRow(1, 2);
+
+        List<Sheet> tables = new ArrayList();
         int quantitySheets = 5;
         for (int i = 0; i < quantitySheets; i++) {
             tables.add(new Sheet("Sheet" + (i + 1)));
             tables.get(i).createSheet();
-        }*/
+        }
+
         schema.showTables();
-        /*Location location = new Location(4, 2);
+
+       // sheet.dropSheet();
+
+        Location location = new Location(4, 2);
         Cell cell1 = new Cell(sheet, location);
-        cell1.setValue("Kate!!!!!!!!!");
-        cell1.updateCell("+Masha!!!!!!", 0);
-        Cell cell2 = new Cell(sheet, "qwerty");*/
-        schema.showData(sheet,3);
+        cell1.setValue("Kate");
+        cell1.updateCell("Masha", 0);
+        Cell cell2 = new Cell(sheet, "qwerty");
+
+        //schema.showData(sheet,3);
 
     }
 }
